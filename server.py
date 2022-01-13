4b6b5e1ae942426d4584de7f32bef78d0e310977
Serena Chandler <serenachandler2019@gmail.com>
2022-01-13 22:14:46 +0000

    began code that allows users to login using google

--- server.py
+++ server.py
@@ -1,15 +1,30 @@
 """Server for restroom app."""
 
-from flask import (Flask, render_template, request, flash, jsonify, session, redirect)
+from flask import (Flask, render_template, request, flash, jsonify, session, url_for, redirect)
 from model import connect_to_db
 import crud
 from jinja2 import StrictUndefined
 import requests
+from authlib.integrations.flask_client import OAuth
 
 app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
+oauth = OAuth(app)
+google = oauth.register(
+name="google",
+client_id='',
+client_secret='',
+access_token_url='https://accounts.google.com/o/oauth2/token',
+access_token_params=None,
+authorize_url='https://accounts.google.com/o/oauth2/auth',
+authorize_params=None,
+api_base_url='https://www.googleapis.com/oauth2/v1/',
+client_kwargs={'scope': 'openid profile email'}
+
+
+)
 
 
 @app.route("/")
@@ -32,13 +47,31 @@ def login_page():
     return redirect("/")
 
 
+@app.route('/google-login')
+def login():
+    google = oauth.create_client('google')
+    redirect_uri = url_for('authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
 
+@app.route('/authorize')
+def authorize():
+    google = oauth.create_client('google')
+    token = google.authorize_access_token()
+    resp = google.get('userinfo')
+    resp.raise_for_status()
+    profile = resp.json()
+    # do something with the token and profile
+    session['email'] = profile['email']
+    return redirect('/')
 
 
 
 @app.route("/logout")
 def logout():
     if session.get("user_id"):
+        session.clear()
+        flash("successfully logged out.")
+    elif session.get('email'):
         session.clear()
         flash("successfully logged out.")
     else:
