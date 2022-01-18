7b2e5b201f6864509988533996d8879615bf0d87
Serena Chandler <serenachandler2019@gmail.com>
2022-01-18 21:57:31 +0000

    added searchbar to react

--- static/js/react.jsx
+++ static/js/react.jsx
@@ -31,4 +31,41 @@ function NavBar() {
   );
 }
 
-ReactDOM.render(<NavBar />, document.querySelector("#ReactApp"));
+function SearchBar() {
+  return (
+    <div>
+        <h1>search for a restroom!</h1>
+      <form action="/handle-search" id="search-form">
+        <p className="searchBarElement">
+          Search: <input id="search-text" type="text" name="search"></input>
+        </p>
+        <input
+          className="searchBarElement"
+          type="checkbox"
+          name="ada"
+          value="True"
+        ></input>
+        <label>ADA accessible</label>
+
+        <p className="searchBarElement">
+          <input
+            value="Find a restroom!"
+            id="search-button"
+            type="submit"
+          ></input>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+function DisplayPage() {
+  return (
+    <React.Fragment>
+      <NavBar />
+      <SearchBar />
+    </React.Fragment>
+  );
+}
+
+ReactDOM.render(<DisplayPage />, document.querySelector("#ReactApp"));
