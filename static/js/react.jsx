3defdeb2325a1371933ffd9a3069633504aecd08
Serena Chandler <serenachandler2019@gmail.com>
2022-01-18 21:38:08 +0000

    added nav bar in react

--- static/js/react.jsx
+++ static/js/react.jsx
@@ -1,13 +1,34 @@
+function NavBar() {
+  return (
+    <header className="header">
+      <nav id="navbar">
+        <div>
+          <a href="/">
+            <img
+              id="flag"
+              src={"static/img/transflagsimple.png"}
+              alt="image of the trans pride flad"
+            />
+          </a>
+        </div>
 
+        <div id="navtext">
+          <h2 className="navtext">
+            <a href="/add">Add a Restroom</a>
+          </h2>
+          <h2 className="navtext">
+            <a href="/user">User Profile</a>
+          </h2>
+          <h2 className="navtext">
+            <a href="/login">Login</a>
+          </h2>
+          <h2 className="navtext">
+            <a href="/logout">Logout</a>
+          </h2>
+        </div>
+      </nav>
+    </header>
+  );
+}
 
-function Hello() {
-    return (
-      <ul>
-        <li>Hi World!</li>
-      </ul>
-    );
-  }
-
-
-
-  ReactDOM.render(<Hello />, document.querySelector('#app'));
+ReactDOM.render(<NavBar />, document.querySelector("#ReactApp"));
