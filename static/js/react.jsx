ca92a3fab11001666df7e92c179dcb15e2a6c688
Serena Chandler <serenachandler2019@gmail.com>
2022-01-18 22:08:25 +0000

    created react-base.html, so all other react pages can stem from it

--- static/js/react.jsx
+++ static/js/react.jsx
@@ -1,35 +1,35 @@
-function NavBar() {
-  return (
-    <header className="header">
-      <nav id="navbar">
-        <div>
-          <a href="/">
-            <img
-              id="flag"
-              src={"static/img/transflagsimple.png"}
-              alt="image of the trans pride flad"
-            />
-          </a>
-        </div>
+// function NavBar() {
+//   return (
+//     <header className="header">
+//       <nav id="navbar">
+//         <div>
+//           <a href="/">
+//             <img
+//               id="flag"
+//               src={"static/img/transflagsimple.png"}
+//               alt="image of the trans pride flad"
+//             />
+//           </a>
+//         </div>
 
-        <div id="navtext">
-          <h2 className="navtext">
-            <a href="/add">Add a Restroom</a>
-          </h2>
-          <h2 className="navtext">
-            <a href="/user">User Profile</a>
-          </h2>
-          <h2 className="navtext">
-            <a href="/login">Login</a>
-          </h2>
-          <h2 className="navtext">
-            <a href="/logout">Logout</a>
-          </h2>
-        </div>
-      </nav>
-    </header>
-  );
-}
+//         <div id="navtext">
+//           <h2 className="navtext">
+//             <a href="/add">Add a Restroom</a>
+//           </h2>
+//           <h2 className="navtext">
+//             <a href="/user">User Profile</a>
+//           </h2>
+//           <h2 className="navtext">
+//             <a href="/login">Login</a>
+//           </h2>
+//           <h2 className="navtext">
+//             <a href="/logout">Logout</a>
+//           </h2>
+//         </div>
+//       </nav>
+//     </header>
+//   );
+// }
 
 function SearchBar() {
   return (
@@ -62,7 +62,7 @@ function SearchBar() {
 function DisplayPage() {
   return (
     <React.Fragment>
-      <NavBar />
+      {/* <NavBar /> */}
       <SearchBar />
     </React.Fragment>
   );
